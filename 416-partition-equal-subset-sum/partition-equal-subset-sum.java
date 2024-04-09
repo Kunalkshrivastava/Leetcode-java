b5e05cdd17b438fc3eb036188a290c33b21e59cc
Kunal Kumar Shrivastava <31282058+Kunalkshrivastava@users.noreply.github.com>
2024-04-09 15:06:43 +0530

    Time: 45 ms (47.59%) | Memory: 57.1 MB (27.38%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,32 +1,32 @@
-
 class Solution {
 
+    private int helper(int ind, int target, int[] nums, int[][] dp) {
+        if(ind >= nums.length)
+            return (target == 0) ? 1 : 0;
+
+        if(dp[ind][target] != -1)
+            return dp[ind][target];
+
+        if(helper(ind+1, target, nums, dp) == 1)
+            return dp[ind][target] = 1;
+
+        if(nums[ind] <= target)
+            return dp[ind][target] = helper(ind+1, target - nums[ind], nums, dp);
+
+        return dp[ind][target] = 0;
+    }
+
     public boolean canPartition(int[] nums) {
-        int sum = 0, n = nums.length;
-        for(int i = 0; i<n ; i++)
+        int sum = 0;
+        for(int i = 0; i<nums.length ; i++)
             sum += nums[i];
 
         if(sum % 2 == 1) return false;
-        int target = sum/2;
 
-        boolean dp[][] = new boolean[n+1][target + 1];
-        for (boolean row[] : dp)
-            Arrays.fill(row, false);
+        int dp[][] = new int[nums.length][(sum/2) + 1];
+        for (int row[] : dp)
+            Arrays.fill(row, -1);
 
-        dp[0][0] = true;
-        for(int i = 1; i<=n ; i++)
-            dp[i][0] = true;
-
-        for(int i = 1; i<=n ; i++) {
-            for(int j = 1; j<= target ; j++) {
-                //inc
-                if(nums[i-1] <= j)
-                    dp[i][j] = dp[i-1][j-nums[i-1]];
-                //exc
-                dp[i][j] = dp[i][j] || dp[i-1][j];
-            }
-        }
-
-        return dp[n][target];
+        return helper(0, sum/2 , nums, dp) == 1 ? true : false;
     }
-}
+}   
